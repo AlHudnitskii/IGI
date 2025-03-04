2565cb0d396793f3c338ae3a04f1294bd30dbd70
alex <alhudnitskii@gmail.com>
2025-03-04 12:19:14 +0300

    LR2 morn 04.03"

---
+++ IGI/LR2/Task3/script.py
@@ -0,0 +1,9 @@
+from geometric_lib import circle
+from geometric_lib import square
+
+a = 10
+
+print('Circle area: ', circle.area(a),
+      '\nCircle perimetr: ', circle.perimetr(a),
+      '\nSquare area: ', square.area(a),
+      '\nSquare perimetr: ', square.perimetr(a))
